f76c76fcd1a7ae376eb254315e24eadf4a2516d0
professor <professor@A290>
2022-03-29 19:22:36 +0200

    Agregar archivos de proyecto.

---
+++ FormMarzo22/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormMarzo22
+{
+    class Class1
+    {
+        static public string cadena = "Pregunta inicial";
+        static public void Funcion1()
+        {
+            cadena = "Segunda pregunta";
+        }
+    }
+}
