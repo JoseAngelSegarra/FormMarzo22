2fbc9669b5d697b14f83cd9681c671df4ca0cddb
Jose Angel <jose@angel.com>
2022-04-05 14:19:27 +0200

    Editamos dos archivos

--- FormMarzo22/Form1.cs
+++ FormMarzo22/Form1.cs
@@ -28,7 +28,7 @@ namespace FormMarzo22
         {
             Class1.Funcion1();
             MessageBox.Show( "adios35");
-            MessageBox.Show("Hola 22");
+            MessageBox.Show("Hola 229");
             //a ver si esto funciona bien
         }
     }
