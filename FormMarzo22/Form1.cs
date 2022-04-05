ce973e560e0d0b9eefab732ee497091dc63de77f
JoseAngelSegarra <jasegarra4@iesperemaria.com>
2022-04-05 19:24:40 +0200

    Commit 3

--- FormMarzo22/Form1.cs
+++ FormMarzo22/Form1.cs
@@ -20,7 +20,7 @@ namespace FormMarzo22
         private void button1_Click(object sender, EventArgs e)
         {
            MessageBox.Show( Class1.cadena);
-            
+            //comentario tercero
 
         }
 
