cb3caa51a17cc7830375a2559d30e72e9b0385be
Jose Angel <jose@angel.com>
2022-04-05 14:39:02 +0200

    kjyfjgf

--- FormMarzo22/Form1.cs
+++ FormMarzo22/Form1.cs
@@ -28,7 +28,7 @@ namespace FormMarzo22
         {
             Class1.Funcion1();
             MessageBox.Show( "adios35");
-            MessageBox.Show("Hola 21");
+            MessageBox.Show("Hola 908767");
             //a ver si esto funciona bien
         }
     }
